2871355f15e3057250aa3358b19c2d3b426e818a
gotpop <liamsobczak@gmail.com>
2017-07-14 20:47:27 +0100

    Course list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { ProfileComponent } from './profile/profile.component';
 import { HomeComponent } from './home/home.component';
 import { LessonListComponent } from './lesson/lesson-list/lesson-list.component';
 import { LessonListDetailComponent } from './lesson/lesson-list-detail/lesson-list-detail.component';
+import { LessonItemComponent } from './lesson/lesson-list/lesson-item/lesson-item.component';
+import { CourseEditComponent } from './course/course-edit/course-edit.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,9 @@ import { LessonListDetailComponent } from './lesson/lesson-list-detail/lesson-li
     ProfileComponent,
     HomeComponent,
     LessonListComponent,
-    LessonListDetailComponent
+    LessonListDetailComponent,
+    LessonItemComponent,
+    CourseEditComponent
   ],
   imports: [
     BrowserModule
