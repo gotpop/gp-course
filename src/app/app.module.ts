7001c54babab285d83d21836fe6c01ec33ade094
gotpop <liamsobczak@gmail.com>
2017-07-14 17:13:33 +0100

    Added basic component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LessonComponent } from './lesson/lesson.component';
+import { CourseComponent } from './course/course.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LessonComponent,
+    CourseComponent,
+    ProfileComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
