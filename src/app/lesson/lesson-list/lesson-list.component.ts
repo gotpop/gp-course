48f6e27c597f0a8e1cff7fadb9f1285dfca29983
gotpop <liamsobczak@gmail.com>
2017-07-14 23:44:35 +0100

    Added model test

--- src/app/lesson/lesson-list/lesson-list.component.ts
+++ src/app/lesson/lesson-list/lesson-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Lesson} from '../../shared/lesson.model';
 
 @Component({
   selector: 'app-lesson-list',
@@ -6,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lesson-list.component.scss']
 })
 export class LessonListComponent implements OnInit {
-
-  constructor() { }
+  lessons: Lesson[] = [
+    new Lesson('Test', 'Test', 'Test')
+  ];
+  constructor() {
+    console.log(this.lessons, 'lessonita')
+  }
 
   ngOnInit() {
   }
