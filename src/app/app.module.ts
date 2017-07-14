63a1beaf2a27bf31709e43a97def8fec26a8b091
gotpop <liamsobczak@gmail.com>
2017-07-14 20:38:37 +0100

    Lesson list com

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { CourseComponent } from './course/course.component';
 import { ProfileComponent } from './profile/profile.component';
 import { HomeComponent } from './home/home.component';
 import { LessonListComponent } from './lesson/lesson-list/lesson-list.component';
-import { LessonListDetailComponent } from './lesson/lesson-list-detail/lesson-item-detail.component';
+import { LessonListDetailComponent } from './lesson/lesson-list-detail/lesson-list-detail.component';
 
 @NgModule({
   declarations: [
--- src/app/lesson/lesson-list-detail/lesson-list-detail.component.ts
+++ src/app/lesson/lesson-list-detail/lesson-list-detail.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-lesson-list-detail',
   templateUrl: './lesson-list-detail.component.html',
-  styleUrls: ['./lesson-item-detail.component.scss']
+  styleUrls: ['./lesson-list-detail.component.scss']
 })
 export class LessonListDetailComponent implements OnInit {
 
