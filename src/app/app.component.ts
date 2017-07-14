85a99582847ade4be3bcb37fa3d47c2955de774c
gotpop <liamsobczak@gmail.com>
2017-07-14 01:24:36 +0100

    Added toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-
+  mashow = false
   maClick() {
       console.log('Ma click"')
+      let ma = this.mashow
+        this.mashow = !ma
   }
 }
