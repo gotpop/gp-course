03d47dbf50bb9ffb6d22812fb4626f229f5090ad
gotpop <liamsobczak@gmail.com>
2017-07-14 20:32:46 +0100

    Edited base models

--- src/app/shared/status.ts
+++ src/app/shared/status.ts
@@ -1,2 +1,15 @@
 export class Status {
+
+  constructor(
+    public name: string,
+    public gender: string,
+    public surname: string,
+    public address1: string,
+    public address2: string,
+    public phone: string,
+    public pref1: string,
+    public pref2: string,
+    public pref3: string
+  ) {  }
+
 }
