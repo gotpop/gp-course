27e7a8361109733fc4605e8da2c984d7afc94aaa
gotpop <liamsobczak@gmail.com>
2017-07-16 14:30:58 +0100

    Added lesson item new logic

--- src/app/lesson/lesson-list-detail/lesson-list-detail.component.ts
+++ src/app/lesson/lesson-list-detail/lesson-list-detail.component.ts
@@ -9,17 +9,19 @@ import {Lesson} from '../../shared/lesson.model';
   styleUrls: ['./lesson-list-detail.component.scss']
 })
 export class LessonListDetailComponent implements OnInit {
-  lessonItems: LessonItem[] = [
-    new LessonItem('Funny youtube video', 'This is a funny video', 'path'),
-    new LessonItem('Info graphic', 'This is a chart', 'path')
-  ];
+  // lessonItems: LessonItem[] = [
+  //   new LessonItem('Funny youtube video', 'This is a funny video', 'path'),
+  //   new LessonItem('Info graphic', 'This is a chart', 'path')
+  // ];
   thisLesson;
+  localLessonItems;
   constructor(private lessonService: LessonService) {}
 
   ngOnInit() {
     this.lessonService.lessonSelected.subscribe(
       ( SubscribedLesson: Lesson ) => {
         this.thisLesson = SubscribedLesson;
+        this.localLessonItems = SubscribedLesson.lessonItems;
         console.log('This is the subscribed lesson: ', this.thisLesson)
       }
     )
--- src/app/shared/lesson.service.ts
+++ src/app/shared/lesson.service.ts
@@ -5,7 +5,13 @@ import {LessonItem} from './lessonItem.model';
 export class LessonService {
   lessonSelected = new EventEmitter<Lesson>();
   private lessons: Lesson[] = [
-    new Lesson('Animals', 'This is a new animal lesson', 'Test', []),
+    new Lesson(
+      'Animals',
+      'This is a new animal lesson',
+      'Test', [
+        new LessonItem('Cat video', 'A video showing cats', 'Test'),
+        new LessonItem('Dog video', 'A video showing dogs', 'Test')
+      ]),
     new Lesson(
       'Geography',
       'This is a geography lesson',
